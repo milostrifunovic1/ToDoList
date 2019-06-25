fac6a84dea5309aa0e1a747856e42856bf0dc295
milostrifunovic1 <milos.trifunovic.92@gmail.com>
2019-06-25 12:11:27 +0200

    Updates

---
+++ precache-manifest.377592b70413e88d670d49787a74ea64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25a64a1257d353b68b7a35050a67f10",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "b3e992ddb835771b4e45",
+    "url": "/ToDoList/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "2e0eef882648b68e271e",
+    "url": "/ToDoList/static/css/main.5cd4c87a.chunk.css"
+  },
+  {
+    "revision": "b3e992ddb835771b4e45",
+    "url": "/ToDoList/static/js/2.28a4341c.chunk.js"
+  },
+  {
+    "revision": "2e0eef882648b68e271e",
+    "url": "/ToDoList/static/js/main.22e4055d.chunk.js"
+  },
+  {
+    "revision": "6a71c43a298729cf07dd",
+    "url": "/ToDoList/static/js/runtime~main.67fadf76.js"
+  }
+]);
